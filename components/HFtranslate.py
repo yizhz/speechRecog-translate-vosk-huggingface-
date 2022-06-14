69827c0ad537dc7e8033f72cb3f78d05ad6cc897
yizhz <59858365+yizhz@users.noreply.github.com>
2022-06-14 10:19:57 +0800

    Update HFtranslate.py

--- components/HFtranslate.py
+++ components/HFtranslate.py
@@ -6,23 +6,29 @@ from suanpan import g
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-@app.param(String(key="param1", alias="from_lang"))
-@app.param(String(key="param2", alias="to_lang"))
+# @app.param(String(key="param1", alias="from_lang"))
+# @app.param(String(key="param2", alias="to_lang"))
+@app.param(String(key="param1", alias="modelPath"))
 @app.afterInit
 def loadModel(context):
     args=context.args
-    from_lang=args['param1']
-    to_lang=args['param2']
-    logger.info('Start downloading the model!')
-    g.tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-"+from_lang+"-"+to_lang)
-    g.model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-"+from_lang+"-"+to_lang)
-    logger.info('Finish downloading the model!')
+    logger.info(args)
+    # from_lang=args['param1']
+    # to_lang=args['param2']
+    # logger.info('Start downloading the model!')
+    g.tokenizer = AutoTokenizer.from_pretrained(args['modelPath'])
+    g.model = AutoModelForSeq2SeqLM.from_pretrained(args['modelPath'])
+    logger.info('Finish loading the model!')
 
+@app.param(String(key="param1", alias="modelPath"))
 @app.input(String(key="inputData1", default="Suanpan"))
 @app.output(String(key="out1", alias="msgout1"))
 def translate(context):
     args=context.args
     text=args.inputData1
+    # g.tokenizer = AutoTokenizer.from_pretrained(args.modelPath)
+    # g.model = AutoModelForSeq2SeqLM.from_pretrained(args.modelPath)
+    # logger.info('Finish loading the model!')
     batch = g.tokenizer.prepare_seq2seq_batch(src_texts=[text], return_tensors='pt', max_length=512)
     translation = g.model.generate(**batch)
     result = g.tokenizer.batch_decode(translation, skip_special_tokens=True)
